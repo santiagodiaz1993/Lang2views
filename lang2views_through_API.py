bf97b2ea9227a6a00443791e571e5512d7353c6f
santiagodiaz1993 <santiagobmx@hotmail.es>
2023-12-19 10:08:04 -0500

    fixed requests problem

--- lang2views_through_API.py
+++ lang2views_through_API.py
@@ -58,6 +58,13 @@ scopes = [
     "https://www.googleapis.com/auth/drive.file",
 ]
 
+# # If modifying these scopes, delete the file token.json.
+SCOPES = [
+    "https://www.googleapis.com/auth/documents",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file",
+]
+
 thefirearmguy_data = {
     "trello_board_id": "64c709c04259bef49a00c5de",
     "trello_longformat_list_ID": "64baa956447059d528377b87",
@@ -176,13 +183,6 @@ def main():
     print(result["text"])
     text = result["text"]
 
-    # # If modifying these scopes, delete the file token.json.
-    SCOPES = [
-        "https://www.googleapis.com/auth/documents",
-        "https://www.googleapis.com/auth/drive",
-        "https://www.googleapis.com/auth/drive.file",
-    ]
-
     """Shows basic usage of the Docs API.
       Prints the title of a sample document.
       """
@@ -212,7 +212,7 @@ def main():
     print(f"Created document with title: {title}, id: {_id}")
 
     # Text insertion
-    requests = [
+    gdoc_text = [
         {
             "insertText": {
                 "location": {
@@ -224,97 +224,69 @@ def main():
     ]
     result = (
         service.documents()
-        .batchUpdate(documentId=_id, body={"requests": requests})
+        .batchUpdate(documentId=_id, body={"requests": gdoc_text})
         .execute()
     )
 
     # This code sample uses the 'requests' library:
     # http://docs.python-requests.org
-    import requests
 
-    url = "https://api.trello.com/1/lists"
+    def create_new_trello_list(name, board):
+        url = "https://api.trello.com/1/lists"
 
-    query = {
-        "name": "{test}",
-        "idBoard": thefirearmguy_data["trello_board_id"],
-        "key": keys["trello_api_key"],
-        "token": keys["trello_token"],
-    }
+        query = {
+            "name": "{test}",
+            "idBoard": thefirearmguy_data["trello_board_id"],
+            "key": keys["trello_api_key"],
+            "token": keys["trello_token"],
+        }
 
-    response = requests.request("POST", url, params=query)
+        response = requests.request("POST", url, params=query)
 
-    print(response.text)
+        print(response.text)
 
-    # url = "https://api.trello.com/1/cards"
+    def clone_trello_card(name, list_id, card_id):
+        url = "https://api.trello.com/1/cards"
 
-    # headers = {"Accept": "application/json"}
+        headers = {"Accept": "application/json"}
 
-    # query = {
-    #     "idList": "657b55f47eb6bf3aede6aa9d",
-    #     "key": "507e54976cbef96c4b8e1b5b883f4639",
-    #     "token": "ATTAc46e0dc1e8b73744994a0538e89bf048d7a1ea0f6a515563a839233e7abdeeb7635B82D5",
-    # }
+        query = {
+            "key": "507e54976cbef96c4b8e1b5b883f4639",
+            "token": "ATTAc46e0dc1e8b73744994a0538e89bf048d7a1ea0f6a515563a839233e7abdeeb7635B82D5",
+            "dsc": "d6d31c3e05df5ae75e15f18f7fd38a00cc0eff6209a01b43b36fbfd88df3dbac",
+            "idCardSource": "64ca6c0f94fc3cee563f130b",
+            "idList": "64baa956447059d528377b87",
+            "keepFromSource": "checklists,attachments,stickers,members,labels,customFields",
+            "name": video_title,
+        }
 
-    # query = {
-    #     "idCardSource": "6371a8b401c60500e4c97a07",
-    #     "idList": "637182618bef42008339d7cf",
-    #     "name": "Hello",
-    #     "keepFromSource": "checklists,attachments,stickers,members,labels,customFields",
-    #     "dsc": "d6d31c3e05df5ae75e15f18f7fd38a00cc0eff6209a01b43b36fbfd88df3dbac",
-    # }
+        response = requests.request("POST", url, headers=headers, params=query)
 
-    # query = {
-    #     "key": "507e54976cbef96c4b8e1b5b883f4639",
-    #     "token": "ATTAc46e0dc1e8b73744994a0538e89bf048d7a1ea0f6a515563a839233e7abdeeb7635B82D5",
-    #     "dsc": "d6d31c3e05df5ae75e15f18f7fd38a00cc0eff6209a01b43b36fbfd88df3dbac",
-    #     "idCardSource": "65121053765103ab051fb04a",
-    #     "idList": "657b55f47eb6bf3aede6aa9d",
-    #     "keepFromSource": "checklists,attachments,stickers,members,labels,customFields",
-    #     "name": "Test Template",
-    # }
+        print(
+            json.dumps(
+                json.loads(response.text),
+                sort_keys=True,
+                indent=4,
+                separators=(",", ": "),
+            )
+        )
 
-    # response = requests.request("POST", url, headers=headers, params=query)
+    def update_customefield_from_card(value, custom_field_id, card_id):
+        url = "https://api.trello.com/1/cards/6580684b86c5ca371989af23/customField/64ca69028b331b036679c2ab/item"
 
-    # print(
-    #     json.dumps(
-    #         json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")
-    #     )
-    # )
+        headers = {"Content-Type": "application/json"}
 
-    # url = "https://api.trello.com/1/cards/657b59e05a409420e8685702/customField/65120a445fd7a02ace295fc1/item"
+        query = {
+            "key": "507e54976cbef96c4b8e1b5b883f4639",
+            "token": "ATTAc46e0dc1e8b73744994a0538e89bf048d7a1ea0f6a515563a839233e7abdeeb7635B82D5",
+        }
 
-    # headers = {"Content-Type": "application/json"}
+        payload = json.dumps({"value": {"text": "Update custom field test"}})
 
-    # query = {
-    #     "key": "507e54976cbef96c4b8e1b5b883f4639",
-    #     "token": "ATTAc46e0dc1e8b73744994a0538e89bf048d7a1ea0f6a515563a839233e7abdeeb7635B82D5",
-    # }
-
-    # payload = json.dumps({"value": {"text": "Update custom field test"}})
-
-    # response = requests.request("PUT", url, data=payload, headers=headers, params=query)
-    # print(response.text)
-
-    # print("the captions are being downloaded")
-    # request = youtube.captions().download(id="yNhJTXABEg0", tfmt="srt")
-
-    # # fh = io.FileIO("YOUR_FILE", "wb")
-
-    # # download = MediaIoBaseDownload(fh, request)
-    # # complete = False
-    # # while not complete:
-    # #     status, complete = download.next_chunk()
-
-    # prankmelater_parent_dir = "/home/santiago/Dropbox/Lang2views/Client Projects/Adley - Prank Me Later/Shorts/"
-    # month_dir = "4. January"
-    # os.mkdir(prankmelater_parent_dir + month_dir)
-    # print("directory opened")
-
-    url_data = urlparse("http://www.youtube.com/watch?v=z_AbfPXTKms&NR=1")
-    query = parse_qs(url_data.query)
-    print(query)
-    video_id = query["v"]
-    print(video_id)
+        response = requests.request(
+            "PUT", url, data=payload, headers=headers, params=query
+        )
+        print(response.text)
 
 
 if __name__ == "__main__":
